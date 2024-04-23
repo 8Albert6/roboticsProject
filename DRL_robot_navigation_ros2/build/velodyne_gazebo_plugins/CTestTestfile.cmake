364944bce2b3314f7e5d32d7e454a4dd27b1b6a6
Alberto Motta <albertomotta@hotmail.it>
2024-04-23 10:36:23 +0000

    Initial commit

--- DRL_robot_navigation_ros2/build/velodyne_gazebo_plugins/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/a/deep-rl-navigation/DRL_robot_navigation_ros2/src/velodyne_simulator/velodyne_gazebo_plugins
-# Build directory: /home/a/deep-rl-navigation/DRL_robot_navigation_ros2/build/velodyne_gazebo_plugins
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- DRL_robot_navigation_ros2/build/velodyne_simulator/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/a/deep-rl-navigation/DRL_robot_navigation_ros2/install/velodyne_description/share/velodyne_description/package.sh"
-. "/home/a/deep-rl-navigation/DRL_robot_navigation_ros2/install/velodyne_gazebo_plugins/share/velodyne_gazebo_plugins/package.sh"
